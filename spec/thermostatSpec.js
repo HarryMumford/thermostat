663fec9819672425c10cdfe2999753f8aaf78da7
Harry Mumford <harry_mumford@hotmail.co.uk>
2019-12-10 17:58:51 +0000

    pass test for temperature increase

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -5,12 +5,18 @@ describe('Thermostat',function() {
 
   beforeEach(function() {
     thermostat = new Thermostat;
-    console.log(thermostat)
   });
   
   describe('it knows its',function() {
     it('starting temperature',function() {
-      expect(thermostat.temperature()).toEqual(20);
+      expect(thermostat.temperature).toEqual(20);
+    });
+  });
+
+  describe('it alters its temperature by',function() {
+    it('increasing',function() {
+      thermostat.increase();
+      expect(thermostat.temperature).toEqual(21);
     });
   });
 
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,7 +1,9 @@
 'use strict';
 
-function Thermostat(){};
+function Thermostat(){
+  this.temperature = 20;
+};
 
-Thermostat.prototype.temperature = function() {
-  return 20;
+Thermostat.prototype.increase = function() {
+  this.temperature ++;
 };
